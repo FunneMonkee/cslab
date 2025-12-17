95312022b0600a0c9c597887378c42e7300c0d8e
FunneMonkee <luismiguel11072000@gmail.com>
2025-12-17 23:56:04 +0000

    esp32 code

--- pt2/mcu/mcu.ino
+++ pt2/mcu/mcu.ino
@@ -1,52 +1,173 @@
-// Sensors
-#define SENSOR_TOP_A     34
-#define SENSOR_TOP_B     35
-#define SENSOR_BOTTOM_A  32
-#define SENSOR_BOTTOM_B  33
-
-// Servos
-#define SERVO_DISPENSE   18
-#define SERVO_LOCK       19
-
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
-Servo dispenseServo;
-Servo lockServo;
+struct LogEntry {
+  long long timestamp;
+  double id;
+};
 
-int capsuleCount = 10;
+//---- WiFi settings
+const char* ssid = "Sousa";
+const char* password = "olaola123";
+//---- MQTT Broker settings
+const char* mqtt_server = "5f479f2b79404567899d28e682008d15.s1.eu.hivemq.cloud";
+const char* mqtt_username = "RaspberryPico";
+const char* mqtt_password = "Raspberrypico2isep";
+
+const char* loginSuccessEsp = "/coffee/esp32/auth/login_success";
+const char* coffeeDispensingEsp = "/coffee/esp32/dispense/requested";
+const char* test = "/test";
+const int mqtt_port = 8883;
+
+//---- Log Size
+const int LOG_SIZE = 100;
+
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 0;
+
+bool isLoggedIn = false;
+
+int logIndex = 0;
+int readIndex = 0;
+int reconnectLoop = 5;
+
+Servo servo;
+
+
+WiFiClientSecure raspClient;
+PubSubClient client(raspClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+char data[100];
+int value = 0;
+bool wifiConnection = true;
+bool wifiReset = false;
 
 void setup() {
-  dispenseServo.attach(SERVO_DISPENSE);
-  lockServo.attach(SERVO_LOCK);
-
-  pinMode(SENSOR_TOP_A, INPUT);
-  pinMode(SENSOR_TOP_B, INPUT);
-  pinMode(SENSOR_BOTTOM_A, INPUT);
-  pinMode(SENSOR_BOTTOM_B, INPUT);
-}
-
-//TODO: create specific server in c that checks the data
-bool redundancyCheck(int a, int b) {
-  return a == b;
-}
-
-void dispenseCapsule() {
-  dispenseServo.write(90);   // open
-  delay(500);
-  dispenseServo.write(0);    // close
+  delay(5000);
+  Serial.begin(9600);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  Serial.println("\n");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  wifiConnection = true;
+  randomSeed(micros());
+  Serial.println("WiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+  while (!Serial) delay(1);
+  raspClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
+  servo.setPeriodHertz(50);
+  servo.attach(25);
 }
 
 void loop() {
-  int topA = digitalRead(SENSOR_TOP_A);
-  int topB = digitalRead(SENSOR_TOP_B);
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiConnection = false;
+    reconnectWiFi();
+  } else {
+    if (!wifiConnection) {
+      Serial.print("[WiFi] Reconnected!\n");
+      wifiConnection = true;
+    }
+    if (reconnectLoop > 0)
+      reconnectLoop--;
+    if (!client.connected() && reconnectLoop == 0) {
+      reconnectLoop = 5;
+      reconnect();
+    }
+    value++;
+  }
 
-  if (!redundancyCheck(topA, topB)) {
-    // safety fault
+
+  client.loop();
+}
+
+unsigned long lastWifiAttempt = 0;
+const unsigned long WIFI_RETRY_INTERVAL = 2000;  // 10s
+
+void reconnectWiFi() {
+  unsigned long now = millis();
+  if (now - lastWifiAttempt < WIFI_RETRY_INTERVAL) {
     return;
   }
+  lastWifiAttempt = now;
 
-  if (capsuleCount > 0) {
-    dispenseCapsule();
-    capsuleCount--;
+  Serial.println("[WiFi] Reconnecting...");
+  WiFi.disconnect();
+  delay(200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+}
+
+void reconnect() {
+  Serial.println("Attempting MQTT connection…");
+  String clientId = "device-raspberry";
+  clientId += String(random(0xffff), HEX);
+  if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+    Serial.println("connected");
+    client.subscribe(loginSuccessEsp);
+    client.subscribe(coffeeDispensingEsp);
+    client.subscribe(test);
+  } else {
+    Serial.print("failed, rc=");
+    Serial.print(client.state());
+    Serial.println(" try again in 5 seconds");
   }
+}
+
+//=======================================
+// This void is called every time we have a message from the broker
+void callback(char* topic, byte* payload, unsigned int length) {
+  if(strcmp(topic, test) == 0){
+    Serial.println(diff_asm(1,1));
+  }
+  else if (strcmp(topic, loginSuccessEsp) == 0) {
+    isLoggedIn = true;
+    Serial.println("Logged in");
+  } else if (strcmp(topic, coffeeDispensingEsp) == 0 && isLoggedIn) {
+    retrieveCoffeeCapsule();
+  }
+  Serial.print("Callback - ");
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+}
+
+void retrieveCoffeeCapsule() {
+  servo.write(180);  // sets the servo position according to the scaled value
+  delay(250);
+  servo.write(0);
+  delay(250);
+}
+
+int diff_asm(int a, int b) {
+  int result;
+
+  asm volatile (
+      "sub %0, %1, %2\n"
+      : "=r"(result)
+      : "r"(a), "r"(b)
+  );
+  return result;
+}
+
+//======================================= publising as string
+void publishMessage(const char* topic, String payload, boolean retained) {
+  if (client.publish(topic, payload.c_str(), true))
+    Serial.println("Message published [" + String(topic) + "]: " + payload);
 }
--- pt2/nfc.ino
+++ pt2/nfc.ino
@@ -1,27 +1,216 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include "UUID.h"
+#include "FS.h"
+#include "SPIFFS.h"
+#include "time.h"
 
-#define SS_PIN  5
-#define RST_PIN 22
+struct LogEntry {
+  long long timestamp;
+  double id;
+};
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+//---- WiFi settings
+const char* ssid = "Sousa";
+const char* password = "olaola123";
+//---- MQTT Broker settings
+const char* mqtt_server = "5f479f2b79404567899d28e682008d15.s1.eu.hivemq.cloud";
+const char* mqtt_username = "ESP32";
+const char* mqtt_password = "Esp32isep";
+
+const int mqtt_port = 8883;
+
+//---- Log Size
+const int LOG_SIZE = 100;
+
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 0;
+
+//------ Topics
+const char* loginAttemptEsp = "/coffee/esp32/auth/login_attempt";
+const char* loginSuccessEsp = "/coffee/esp32/auth/login_success";
+const char* coffeeDispensingEsp = "/coffee/esp32/dispense/requested";
+
+//------ IDs
+const char* id = "esp32";
+
+
+//------ Flags
+
+bool isLoggedIn = false;
+
+int logIndex = 0;
+int readIndex = 0;
+int reconnectLoop = 5;
+
+const int authButtonPin = 25;
+const int coffeeButtonPin = 32;
+
+int authButtonState = 0;
+int coffeeButtonState = 0;
+
+UUID uuid;
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+char data[100];
+int value = 0;
+bool wifiConnection = true;
+bool wifiReset = false;
 
 void setup() {
-  SPI.begin();
-  mfrc522.PCD_Init();
+  delay(5000);
+  Serial.begin(9600);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  Serial.println("\n");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  wifiConnection = true;
+  randomSeed(micros());
+  Serial.println("WiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+  while (!Serial) delay(1);
+  uuid.seed(2);
+  espClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
+  pinMode(authButtonPin, INPUT_PULLUP);
+  pinMode(coffeeButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  if (!mfrc522.PICC_IsNewCardPresent()) return;
-  if (!mfrc522.PICC_ReadCardSerial()) return;
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiConnection = false;
+    reconnectWiFi();
+  } else {
+      if (!wifiConnection) {
+        Serial.print("[WiFi] Reconnected!\n");
+        wifiConnection = true;
+      }
+      if (reconnectLoop > 0)
+        reconnectLoop--;
+      if (!client.connected() && reconnectLoop == 0) {
+        reconnectLoop = 5;
+        reconnect();
+      }
+      client.loop();
+      value++;
+    }
+  if (digitalRead(authButtonPin) == HIGH) {
+    char timestamp[32];
+    isLoggedIn = true;
+    // turn LED on:
+    Serial.println("NFC Login");
+    StaticJsonDocument<128> doc;
 
-  String uid = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    uid += String(mfrc522.uid.uidByte[i], HEX);
+    uuid.generate();
+
+    getLocTime().toCharArray(timestamp, sizeof(timestamp));
+    doc["timestamp"] = timestamp;
+    doc["id"] = id;
+    doc["uuid"] = uuid;
+    doc["severity"] = "INFO";
+    doc["source"] = id;
+    doc["eventType"] = "authentication";
+    doc["userId"] = 1;
+    uint8_t jsonBuffer[1024];
+    serializeJson(doc, (char*)jsonBuffer, sizeof(jsonBuffer));
+    publishMessage(loginAttemptEsp, (char*)jsonBuffer, true);
+    publishMessage(loginSuccessEsp, (char*)jsonBuffer, true);
+    while (isLoggedIn) {
+      Serial.println("Choose coffee");
+      if (digitalRead(coffeeButtonPin) == HIGH) {
+        doc["timestamp"] = timestamp;
+        doc["id"] = id;
+        doc["uuid"] = uuid;
+        doc["severity"] = "INFO";
+        doc["source"] = id;
+        doc["eventType"] = "coffeeDispensing";
+        doc["flavor"] = "BlackCoffeeNespresso";
+        serializeJson(doc, (char*)jsonBuffer, sizeof(jsonBuffer));
+        publishMessage(coffeeDispensingEsp, (char*)jsonBuffer, true);
+        Serial.println("Coffee chosen");
+        isLoggedIn = false;
+      }
+      delay(100);
+    }
+    client.loop();
+  } else {
+    Serial.println("Waiting for Login");
   }
+  client.loop();
+  delay(100);
+}
 
-  //TODO: send UID to auth server
-  Serial.println(uid);
+unsigned long lastWifiAttempt = 0;
+const unsigned long WIFI_RETRY_INTERVAL = 2000;  // 10s
 
-  delay(1000);
+void reconnectWiFi() {
+  unsigned long now = millis();
+  if (now - lastWifiAttempt < WIFI_RETRY_INTERVAL) {
+    return;
+  }
+  lastWifiAttempt = now;
+
+  Serial.println("[WiFi] Reconnecting...");
+  WiFi.disconnect();
+  delay(200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+}
+
+
+void reconnect() {
+  Serial.println("Attempting MQTT connection…");
+  String clientId = "device-esp32";
+  clientId += String(random(0xffff), HEX);
+  if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+    Serial.println("connected");
+  } else {
+    Serial.print("failed, rc=");
+    Serial.print(client.state());
+    Serial.println(" try again in 5 seconds");
+  }
+}
+
+String getLocTime() {
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  if (now.tv_sec < 100000) {
+    return "";
+  }
+  struct tm timeinfo;
+  localtime_r(&now.tv_sec, &timeinfo);
+  char buf[64];
+  strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+  return String(buf);
+}
+
+//=======================================
+// This void is called every time we have a message from the broker
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Callback - ");
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+}
+
+//======================================= publising as string
+void publishMessage(const char* topic, String payload, boolean retained) {
+  if (client.publish(topic, payload.c_str(), true))
+    Serial.println("Message published [" + String(topic) + "]: " + payload);
 }
