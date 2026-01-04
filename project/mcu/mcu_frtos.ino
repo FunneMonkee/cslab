65d2a96656f4812705726ed63062c6e6296ff5a8
FunneMonkee <luismiguel11072000@gmail.com>
2026-01-04 16:58:21 +0000

    working servo

--- project/mcu/mcu_frtos.ino
+++ project/mcu/mcu_frtos.ino
@@ -1,139 +1,159 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
-#include "time.h"
 
-const char* ssid = "Sousa";
-const char* password = "olaola123";
+// Log settings
+struct LogEntry {
+  long long timestamp;
+  double id;
+};
+const int LOG_SIZE = 100;
 
-const char* mqtt_server   = "5f479f2b79404567899d28e682008d15.s1.eu.hivemq.cloud";
+// WiFi settings
+const char* ssid = "Pinto";
+const char* password = "uJM9X8zG2q";
+
+// MQTT Broker settings
+const char* mqtt_server = "5f479f2b79404567899d28e682008d15.s1.eu.hivemq.cloud";
 const char* mqtt_username = "RaspberryPico";
 const char* mqtt_password = "Raspberrypico2isep";
-const int   mqtt_port     = 8883;
+const int mqtt_port = 8883;
+WiFiClientSecure mqttClient;
+PubSubClient client(mqttClient);
 
-const char* loginSuccessEsp     = "/coffee/esp32/auth/login_success";
+// MQTT Topics
+const char* loginSuccessEsp = "/coffee/esp32/auth/login_success";
 const char* coffeeDispensingEsp = "/coffee/esp32/dispense/requested";
-//keep for now
-const char* testTopic           = "/test";
+const char* test = "/test";
 
+// Time settings
 const long gmtOffset_sec = 0;
 const int daylightOffset_sec = 0;
 
-WiFiClientSecure raspClient;
-PubSubClient client(raspClient);
+// Declare task handle
+TaskHandle_t MoveServoTaskHandle = NULL;
+TaskHandle_t MQTTReconnectTaskHandle = NULL;
 
+// Semaphores
+SemaphoreHandle_t moveServoTaskSem = NULL;
+
+// Servos
 Servo servo;
 
-bool isLoggedIn = false;
+// Global vars
+bool isLoggedIn = true;
+int logIndex = 0;
+int readIndex = 0;
+int reconnectLoop = 5;
+bool wifiConnection = false;
 
-SemaphoreHandle_t loginSem;
-SemaphoreHandle_t coffeeSem;
-SemaphoreHandle_t testSem;
+// Tasks
+//TODO: wifi reconnect hogged both cores (even with higher prio servo task)
 
-void retrieveCoffeeCapsule() {
-  servo.write(180);
-  vTaskDelay(pdMS_TO_TICKS(250));
-  servo.write(0);
-  vTaskDelay(pdMS_TO_TICKS(250));
+void MQTTReconnectTask(void* parameter) {
+  for (;;) {
+    if (!client.connected()) {
+      Serial.println("Attempting MQTT connection…");
+      String clientId = "device-raspberry";
+      clientId += String(random(0xffff), HEX);
+      if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+        Serial.println("connected");
+        client.subscribe(loginSuccessEsp);
+        client.subscribe(coffeeDispensingEsp);
+        client.subscribe(test);
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+      }
+    }
+
+    client.loop();                  
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
 }
 
-int diff_asm(int a, int b) {
-  int result;
-  asm volatile (
-    "sub %0, %1, %2\n"
-    : "=r"(result)
-    : "r"(a), "r"(b)
-  );
-  return result;
+void MoveServoTask(void* parameter) {
+  for (;;) {
+    if (xSemaphoreTake(moveServoTaskSem, portMAX_DELAY)) {
+      Serial.println("moving servo..");
+      servo.write(180);
+      delay(250);
+      servo.write(0);
+      delay(250);
+    }
+  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  if (strcmp(topic, loginSuccessEsp) == 0) {
-    xSemaphoreGive(loginSem);
-  } else if (strcmp(topic, coffeeDispensingEsp) == 0) {
-    xSemaphoreGive(coffeeSem);
-  } else if (strcmp(topic, testTopic) == 0) {
-    xSemaphoreGive(testSem);
+  Serial.println("callback");
+  if (strcmp(topic, test) == 0) {
+    //Serial.println(diff_asm(1, 1));
+  } else if (strcmp(topic, loginSuccessEsp) == 0) {
+    isLoggedIn = true;
+    Serial.println("Logged in");
+  } else if (strcmp(topic, coffeeDispensingEsp) == 0 && isLoggedIn) {
+    Serial.println("give sem");
+    xSemaphoreGive(moveServoTaskSem);
   }
-}
-
-void wifiMqttTask(void* pvParameters) {
-  for (;;) {
-    if (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        vTaskDelay(pdMS_TO_TICKS(500));
-      }
-    }
-
-    if (!client.connected()) {
-      String clientId = "device-raspberry-" + String(random(0xffff), HEX);
-      if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
-        client.subscribe(loginSuccessEsp);
-        client.subscribe(coffeeDispensingEsp);
-        client.subscribe(testTopic);
-      }
-    }
-
-    client.loop();
-    vTaskDelay(pdMS_TO_TICKS(10));
+  Serial.print("Callback - ");
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
   }
-}
-
-void loginTask(void* pvParameters) {
-  for (;;) {
-    if (xSemaphoreTake(loginSem, portMAX_DELAY)) {
-      isLoggedIn = true;
-    }
-  }
-}
-
-void coffeeTask(void* pvParameters) {
-  for (;;) {
-    if (xSemaphoreTake(coffeeSem, portMAX_DELAY)) {
-      if (!isLoggedIn) continue;
-      retrieveCoffeeCapsule();
-      isLoggedIn = false;
-    }
-  }
-}
-
-void testTask(void* pvParameters) {
-  for (;;) {
-    if (xSemaphoreTake(testSem, portMAX_DELAY)) {
-      Serial.println(diff_asm(1, 1));
-    }
-  }
+  Serial.println("");
 }
 
 void setup() {
-  Serial.begin(9600);
-  delay(2000);
-
+  delay(5000);
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  Serial.println("\n");
   WiFi.mode(WIFI_STA);
-
-  //TODO: change to esp32 api (but this works somehow)
-  raspClient.setInsecure();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  wifiConnection = true;
+  randomSeed(micros());
+  Serial.println("WiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+  while (!Serial) delay(1);
+  mqttClient.setInsecure();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-
-  configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
-
+  configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
   servo.setPeriodHertz(50);
-  servo.attach(25);
+  servo.attach(23);
 
-  loginSem  = xSemaphoreCreateBinary();
-  coffeeSem = xSemaphoreCreateBinary();
-  testSem   = xSemaphoreCreateBinary();
+  moveServoTaskSem = xSemaphoreCreateBinary();
 
-  xTaskCreatePinnedToCore(wifiMqttTask, "WiFiMQTT", 4096, NULL, 3, NULL, 0);
-  xTaskCreatePinnedToCore(loginTask,   "LoginTask", 2048, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(coffeeTask,  "CoffeeTask",2048, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(testTask,    "TestTask",  2048, NULL, 1, NULL, 1);
+  // Network in Core 0
+  xTaskCreatePinnedToCore(
+    MQTTReconnectTask,
+    "MQTTTReconnectTask",
+    10000,
+    NULL,
+    1,
+    &MQTTReconnectTaskHandle,
+    0);
+
+  xTaskCreatePinnedToCore(
+    MoveServoTask,         // Task function
+    "MoveServoTask",       // Task name
+    10000,                 // Stack size (bytes)
+    NULL,                  // Parameters
+    1,                     // Priority
+    &MoveServoTaskHandle,  // Task handle
+    1                      // Core 1
+  );
 }
 
 void loop() {
-  vTaskDelay(portMAX_DELAY);
+  // Empty because FreeRTOS scheduler runs the task
 }
