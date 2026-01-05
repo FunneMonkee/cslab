cdace816f74c85c37b5909b2139f425b5ff41fee
Ricardo Sousa <oxy@MacBook-Pro-de-Ricardo.local>
2026-01-05 22:05:58 +0000

    nfc: nfc reading implemented

--- project/rest_api/src/main.rs
+++ project/rest_api/src/main.rs
@@ -30,7 +30,7 @@ async fn main() -> std::io::Result<()> {
             .service(update_user)
             .service(delete_user)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
