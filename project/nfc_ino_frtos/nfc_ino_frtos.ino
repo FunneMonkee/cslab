aebbb5dafbe64955ce8f91b22c8f40068e033adc
Ricardo Sousa <oxy@MacBook-Pro-de-Ricardo.local>
2026-01-07 01:41:48 +0000

    nfc topics covered

--- project/nfc_ino_frtos/nfc_ino_frtos.ino
+++ project/nfc_ino_frtos/nfc_ino_frtos.ino
@@ -21,10 +21,14 @@ const int   mqtt_port     = 8883;
 
 const char* loginAttemptEsp     = "/coffee/esp32/auth/login_attempt";
 const char* loginSuccessEsp     = "/coffee/esp32/auth/login_success";
+const char* loginFailedEsp     = "/coffee/esp32/auth/login_failed";
+const char* logoutEsp     = "/coffee/esp32/auth/logout";
 const char* coffeeDispensingEsp = "/coffee/esp32/dispense/requested";
 
-const char* authAttemptEvent = "AuthenticationAttempt";
-const char* authSuccessEvent = "AuthenticationSuccess";
+const char* loginAttemptEvent = "LoginAttempt";
+const char* loginSuccessEvent = "LoginSuccess";
+const char* logoutEvent = "Logout";
+const char* loginFailedEvent = "LoginFailed";
 
 const char* deviceId = "esp32";
 
@@ -98,23 +102,7 @@ void IRAM_ATTR coffeeButtonISR() {
   }
 }
 
-void wifiMqttTask(void* pvParameters) {
-  for (;;) {
-    if (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        vTaskDelay(pdMS_TO_TICKS(500));
-      }
-    }
-    if (!client.connected()) {
-      String clientId = "esp32-" + String(random(0xffff), HEX);
-      client.connect(clientId.c_str(), mqtt_username, mqtt_password);
-    }
-    client.loop();
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
+////////////////////////////////////////////////////////////////// AUTH //////////////////////////////////////////////////////////////////////////
 bool processCardRead() {
   if (!mfrc522.PICC_IsNewCardPresent()) return false;
   if (!mfrc522.PICC_ReadCardSerial()) return false;
@@ -141,10 +129,7 @@ bool processCardRead() {
   companyId = ((uint16_t)buffer[0] << 8) | buffer[1];
   Serial.print("CompanyID read from card: ");
   Serial.println(companyId);
-  const auto doc = generateAuthJson(authAttemptEvent);
-  char bufferJson[256];
-  serializeJson(doc, bufferJson);
-  publishMessage(loginAttemptEsp, bufferJson);
+  loginAttempt();
   return verifyCompanyID();
 }
 
@@ -159,6 +144,7 @@ bool verifyCompanyID(){
       return checkCardPayload(http.getString());
     } else {
       Serial.println("HTTP GET failed");
+      loginFailed("TIMEOUT");
     }
     http.end();
   }
@@ -166,7 +152,7 @@ bool verifyCompanyID(){
 }
 
 bool checkCardPayload(const String payload) {
-  JsonDocument doc;
+  StaticJsonDocument<512> doc;
   DeserializationError error = deserializeJson(doc, payload);
 
   Serial.println(payload);
@@ -184,7 +170,7 @@ bool checkCardPayload(const String payload) {
   char * endString;
 
   if(nfc_id == NULL || permission == NULL){
-    Serial.println("Error in json");
+    loginFailed("INVALID_CARD");
     return false;
   }
 
@@ -201,35 +187,76 @@ bool checkCardPayload(const String payload) {
   return true;
 }
 
-JsonDocument generateAuthJson(const char* event) {
-    JsonDocument doc;
-    uuid.generate();
-    doc["timestamp"] = getLocTime();
-    doc["id"] = deviceId;
-    doc["uuid"] = uuid.toCharArray();
-    doc["severity"] = "INFO";
-    doc["eventType"] = event;
-    doc["userId"] = companyId;
-    return doc;
+void loginAttempt(){
+  StaticJsonDocument<64> payloadDoc;
+  JsonArray payload = payloadDoc.to<JsonArray>();
+
+  payload.add(companyId);
+  processJsonAndPublishTopic(loginAttemptEvent, "INFO", loginAttemptEsp, payload);
 }
 
-bool getLoginFlagValue()
-{
+void login(){
+  StaticJsonDocument<64> payloadDoc;
+  JsonArray payload = payloadDoc.to<JsonArray>();
+
+  payload.add(companyId);
+  processJsonAndPublishTopic(loginSuccessEvent, "INFO", loginSuccessEsp,payload);
+  setLoginFlagValue(true);
+}
+
+void logout() {
+  StaticJsonDocument<64> payloadDoc;
+  JsonArray payload = payloadDoc.to<JsonArray>();
+
+  payload.add(companyId);
+  processJsonAndPublishTopic(logoutEvent, "INFO", logoutEsp, payload);
+  setLoginFlagValue(false);
+}
+
+void loginFailed(const char* reason) {
+  StaticJsonDocument<64> payloadDoc;
+  JsonArray payload = payloadDoc.to<JsonArray>();
+
+  payload.add(companyId);
+  payload.add(reason);
+  processJsonAndPublishTopic(loginFailedEvent, "WARN", loginFailedEsp, payload);
+}
+
+void processJsonAndPublishTopic(const char* event, const char* severity, const char* topic, const JsonArray payload) {
+  DynamicJsonDocument doc = generateAuthJson(event, severity, payload);
+  char buffer[512];
+  serializeJson(doc, buffer);
+  publishMessage(topic, buffer);
+}
+
+bool getLoginFlagValue() {
   portENTER_CRITICAL(&mux);
   bool v = isLoggedIn;
   portEXIT_CRITICAL(&mux);
   return v;
 }
 
+DynamicJsonDocument generateAuthJson(
+  const char* event,
+  const char* severity,
+  JsonArray payload) {
+  DynamicJsonDocument doc(512);
+  uuid.generate();
+  doc["timestamp"] = getLocTime();
+  doc["id"] = deviceId;
+  doc["uuid"] = uuid.toCharArray();
+  doc["severity"] = severity;
+  doc["eventType"] = event;
+  doc["payload"].set(payload); 
+
+  return doc;
+}
+
 void authTask(void* pvParameters) {
   for (;;) {
     if(!getLoginFlagValue()){
      if(processCardRead()){
-        const auto doc = generateAuthJson(authSuccessEvent);
-        char buffer[256];
-        serializeJson(doc, buffer);
-        publishMessage(loginSuccessEsp, buffer);
-        setLoginFlagValue(true);
+        login();
         xTimerStart(loginTimer, 0);
       }
       cleanup();
@@ -238,12 +265,14 @@ void authTask(void* pvParameters) {
   }
 }
 
+//////////////////////////////////////////// COFFEE /////////////////////////////////////////////////////
+
 void coffeeTask(void* pvParameters) {
   for (;;) {
     if (xSemaphoreTake(coffeeButtonSem, portMAX_DELAY)) {
       Serial.println("Houve interrupção");
       if (getLoginFlagValue()){
-        JsonDocument doc;
+        StaticJsonDocument<256> doc;
         doc["timestamp"] = getLocTime();
         doc["id"] = deviceId;
         doc["severity"] = "INFO";
@@ -252,13 +281,37 @@ void coffeeTask(void* pvParameters) {
         char buffer[256];
         serializeJson(doc, buffer);
         publishMessage(coffeeDispensingEsp, buffer);
-        setLoginFlagValue(false);
+        logout();
         xTimerStop(loginTimer, 0);
       }
     }
     vTaskDelay(pdMS_TO_TICKS(10));
   }
 }
+
+////////////////////////////////////////////////////////////////// WIFI //////////////////////////////////////////////////////////////////////////
+
+void wifiMqttTask(void* pvParameters) {
+  for (;;) {
+    if (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(pdMS_TO_TICKS(500));
+      }
+    }
+    if (!client.connected()) {
+      String clientId = "esp32-" + String(random(0xffff), HEX);
+      client.connect(clientId.c_str(), mqtt_username, mqtt_password);
+    }
+    client.loop();
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////// HELPER FUNCTIONS ///////////////////////////////////////////////////////////////////
+
 
 void cleanup() {
   mfrc522.PICC_HaltA();
@@ -274,7 +327,7 @@ void configCardKey() {
 
 void timerCallback(TimerHandle_t xTimer){
   Serial.println("Timer callback executed");
-  setLoginFlagValue(false);
+  logout();
   Serial.print("Login value: ");
   Serial.println(getLoginFlagValue());
 }
@@ -306,7 +359,6 @@ void setup() {
 
   attachInterrupt(coffeeButtonPin, coffeeButtonISR, RISING);
 
-  //TODO: decide on stack size
   xTaskCreatePinnedToCore(wifiMqttTask, "WiFiMQTT", 8192, NULL, 3, NULL, 0);
   xTaskCreatePinnedToCore(authTask, "AuthTask", 12288, NULL, 2, NULL, 1);
   xTaskCreatePinnedToCore(coffeeTask, "CoffeeTask", 4096, NULL, 2, NULL, 1);
